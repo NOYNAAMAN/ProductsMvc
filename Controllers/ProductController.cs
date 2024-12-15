92de9a443c452f91df54588a082b4bc6041eaf4d
Noy <noynaaman114258@gmail.com>
2024-12-15 18:45:46 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ProductsMvc.Infarstucture.Data;
+using ProductsMvc.Models;
+
+namespace ProductsMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductMvcContext _context;
+
+        public ProductController(ProductMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(_context.Products.ToList());
+        }
+
+
+        public IActionResult Details(int id)
+        {
+
+            Product? p = _context.Products.FirstOrDefault(p => p.Id == id);
+            if (p == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(p);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            _context.Products.Add(p);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Infarstucture/Data/ProductMvcContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsMvc.Models;
+
+namespace ProductsMvc.Infarstucture.Data
+{
+    public class ProductMvcContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        public ProductMvcContext(DbContextOptions dbOptions) : base(dbOptions)
+        {
+        }
+    }
+
+}
